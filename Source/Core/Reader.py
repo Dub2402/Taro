a4278716ab78535ab38023676d3456cff551ea76
Irina Dub <109979502+Dub2402@users.noreply.github.com>
2025-07-13 17:50:21 +0300

    Refactoring

--- Source/Core/Reader.py
+++ Source/Core/Reader.py
@@ -1,5 +1,8 @@
 import pandas
 
+from os import PathLike
+from typing import Iterable
+
 class Reader:
 
 	#==========================================================================================#
@@ -7,34 +10,40 @@ class Reader:
 	#==========================================================================================#
 
 	@property
-	def letters(self):
-		"""Список посланий."""
-		return self.__ReadExcel(self.__Settings["letters"], column = "Послания")
+	def letters(self) -> tuple[str]:
+		"""Кортеж наставлений."""
+
+		return self.__LettersDict["Наставления"]
 	
 	@property
-	def appeals(self):
-		"""Список призывов."""
-		return self.__ReadExcel(self.__Settings["letters"], column = "Призывы")
+	def appeals(self) -> tuple[str]:
+		"""Кортеж призывов."""
+
+		return self.__LettersDict["Призывы"]
+		
+	@property
+	def StraightCard(self) -> tuple[str]:
+		"""Кортеж названий прямых карт."""
+
+		return self.__YesNoDict["Обычные карты"]
+
+	@property
+	def ReversedCard(self) -> tuple[str]:
+		"""Кортеж названий перевёрнутых карт."""
+
+		return self.__YesNoDict["Перевернутые карты"]
 	
 	@property
-	def StraightCard(self):
-		"""Список названий прямых карт."""
-		return self.__ReadExcel(self.__Settings["yes_no"], column = "Обычные карты")
+	def StraightValues(self) -> tuple[str]:
+		"""Кортеж значений прямых карт."""
+
+		return self.__YesNoDict["Значения обычных карт"]
 	
 	@property
-	def ReversedCard(self):
-		"""Список названий перевёрнутых карт."""
-		return self.__ReadExcel(self.__Settings["yes_no"], column = "Перевернутые карты")
-	
-	@property
-	def StraightValues(self):
-		"""Список значений прямых карт."""
-		return self.__ReadExcel(self.__Settings["yes_no"], column = "Значения обычных карт")
-	
-	@property
-	def ReversedValues(self):
-		"""Список значений перевёрнутых карт."""
-		return self.__ReadExcel(self.__Settings["yes_no"], column = "Значения перевернутых карт")
+	def ReversedValues(self) -> tuple[str]:
+		"""Кортеж значений перевёрнутых карт."""
+
+		return self.__YesNoDict["Значения перевернутых карт"]
 	
 	def __init__(self, Settings: dict):
 
@@ -45,31 +54,32 @@ class Reader:
 		:type Settings: dict
 		"""
 
-		#---> Генерация динамических атрибутов.
-		#==========================================================================================#
-
-		self.__Settings = Settings
+		self.__LettersDict = self.__ReadExcel(Settings["letters"])
+		self.__YesNoDict = self.__ReadExcel(Settings["yes_no"])
 
 	#==========================================================================================#
 	# >>>>> ПРИВАТНЫЕ МЕТОДЫ <<<<< #
 	#==========================================================================================#
-
-	def __ReadExcel(self, path_file: str, column: str) -> list:
-		"""
-		Чтение колонки excel-файла.
-
-		:param path_file: Путь к файлу.
-		:type path_file: str
-		:param column: Название колонки.
-		:type column: str
-		:return: Тексты в строках колонки.
-		:rtype: list
-		"""
-		
-		exceldata = pandas.read_excel(path_file)
-		Products = pandas.DataFrame(exceldata, columns=[column])
-		reading_data = Products[column].tolist()
-		reading_data = list(filter(lambda x: str(x) != "nan", reading_data))
 	
-		return reading_data
-	
+	def __ReadExcel(self, path: PathLike) -> dict:
+		"""
+		Считывает файл Excel и интерпретирует его в словарь.
+
+		:param path: Путь к файлу.
+		:type path: PathLike
+		:return: Словарное представление.
+		:rtype: dict
+		"""
+
+		Data = pandas.read_excel(path, dtype = str)
+		Data = Data.fillna("")
+		DataDictionary: dict[str, Iterable[str]] = Data.to_dict(orient = "list")
+
+		for Column in DataDictionary.keys(): DataDictionary[Column] = tuple(filter(lambda Value: Value, DataDictionary[Column]))
+
+		for Column in DataDictionary.keys():
+			Buffer = list()
+			for Item in DataDictionary[Column]: Buffer.append(Item.strip())
+			DataDictionary[Column] = tuple(Buffer)
+
+		return DataDictionary
